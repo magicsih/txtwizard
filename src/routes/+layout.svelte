03b41b1e55c43d105bae13701dfe1e535055e710
Ilhwan Seo <wellknowngeek@gmail.com>
2024-10-10 19:47:11 +0900

    Refactor i18n-ally settings to include additional localization keys

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
+<script lang="ts">
+	import { t } from 'svelte-i18n'
+</script>
+
 <nav>
 	<button class="menu-toggle" aria-label="Toggle navigation">☰</button>
 	<div class="nav-links">
-		<a href="/">Home</a>
-		<a href="/encryption">Encrypt</a>
-		<a href="/decryption">Decrypt</a>
-		<a href="/encoding-decoding">Encoding</a>
-		<a href="/hashing">Hashing</a>
-		<a href="/compression">Compression</a>
-        <a href="/ethereum-key-generation">KeyGen(ETH)</a>
+		<a href="/">{ $t('home') }</a>
+		<a href="/encryption">{$t("encryption")}</a>
+		<a href="/decryption">{$t("decryption")}</a>
+		<a href="/encoding-decoding">{ $t('encoding') }</a>
+		<a href="/hashing">{ $t('hashing') }</a>
+		<a href="/compression">{ $t('compression') }</a>
+        <a href="/key-generation">{ $t('keygen') }</a>
 	</div>
 </nav>
 <slot />
