db7e52a3b7d1671ef16e83035b4108f5ed2d36a1
Ilhwan Seo <magicsih@gmail.com>
2024-10-31 14:47:44 +0900

    Add additional localization support for German, Spanish, French, Hindi, Italian, Japanese, Dutch, Polish, Portuguese, Russian, Turkish, and Chinese

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -6,6 +6,18 @@ const defaultLocale = 'en'
 
 register('en', () => import('./locales/en.json'))
 register('ko', () => import('./locales/ko.json'))
+register('de', () => import('./locales/de.json'))
+register('es', () => import('./locales/es.json'))
+register('fr', () => import('./locales/fr.json'))
+register('hi', () => import('./locales/hi.json'))
+register('it', () => import('./locales/it.json'))
+register('ja', () => import('./locales/ja.json'))
+register('nl', () => import('./locales/nl.json'))
+register('pl', () => import('./locales/pl.json'))
+register('pt', () => import('./locales/pt.json'))
+register('ru', () => import('./locales/ru.json'))
+register('tr', () => import('./locales/tr.json'))
+register('zh', () => import('./locales/zh.json'))
 
 init({
     fallbackLocale: defaultLocale,
